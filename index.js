cee057699ee673d7dfeb5510bc440489094aeba6
Nguyen Thanh <h1.tnguyen@pm.me>
2024-09-06 00:36:00 +0700

    Install PUG and create views/

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("views", "./views"); //Set views directory
+app.set("view engine", "pug"); //Set views engine is 'pug'
+
 app.get("/", (req, res) => {
-  res.send("Homepage");
+  res.render("./client/pages/home/index");
 });
 
 app.get("/products", (req, res) => {
-  res.send("Product Category");
+  res.render("./client/pages/products/index");
 });
 
 app.listen(port, () => {
