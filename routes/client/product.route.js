6f4f8d2db200092f32075ba58b42ac4162bd4500
tnguyen-2002 <h1.tnguyen088@gmail.com>
2024-09-10 15:45:24 +0700

    Update product.route.js

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router  = express.Router();
 
 router.get("/", (req,res) => {
-    res.render("clint/pages/products/index");
+    res.render("client/pages/products/index");
 });
 
 module.exports = router;
