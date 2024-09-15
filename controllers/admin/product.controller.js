8168635eacb4212066d3ee3278ac167339e86ffc
tnguyen-2002 <h1.tnguyen088@gmail.com>
2024-09-15 13:09:59 +0700

    Add product in database to admin product page

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,13 @@
-module.exports.index = (req,res) => {
+const Product = require("../../models/product.model")
+
+module.exports.index = async (req,res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+    
+    
     res.render("admin/pages/product/index", {
-        pageTitle: "Product Category"
+        pageTitle: "Product Category",
+        products: products
     });
 }
