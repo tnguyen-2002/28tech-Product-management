e9265fd305ac698b6d2658f84588736c288ab41f
rei666 <h1.tnguyen088@gmail.com>
2024-10-22 23:25:39 +0700

    [admin] edit product feature

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-//Flash - https://github.com/RGBboy/express-flash
-const flash = require("express-flash");
+const flash = require("express-flash");  //Flash - https://github.com/RGBboy/express-flash
 const cookieParser = require("cookie-parser"); // - https://github.com/expressjs/cookie-parser
 const session = require("express-session"); //- https://github.com/expressjs/session
 const systemConfig = require("./config/system");
@@ -16,11 +15,15 @@ database.connect();
 
 const routeAdmin = require("./routes/admin/index.route.js");
 const routeClient = require("./routes/client/index.route.js");
+const methodOverride = require("method-override");
 
 app.set("views", "./views"); //Set views directory
 app.set("view engine", "pug"); //Set views engine is 'pug'
 
 app.use(express.static("public")); //Set static folder
+
+//override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 //Flash 
 app.use(cookieParser("ABCZYX"));
