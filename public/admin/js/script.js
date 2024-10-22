94a2be0998841818a5c59d426e3d939f6b04538e
rei666 <h1.tnguyen088@gmail.com>
2024-10-22 22:19:06 +0700

    [Admin] Add new product - preview image

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -221,3 +221,17 @@ if(alertMessage) {
   }, 3000);
 }
 //end alert timeout 
+
+// Preview image
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+  uploadImageInput.addEventListener("change", () => {
+    const file = uploadImageInput.files[0];
+    if(file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End preview image
